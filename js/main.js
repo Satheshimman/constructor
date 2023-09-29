97ccd86e432cfc01d3e496f744bb3c5e9b661d33
satheshimman <satheshimman17@gmail.com>
2023-09-29 20:52:07 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,29 @@
+{
+    class Details_first {
+        constructor(value){
+            this.a=value
+           
+        }
+        send(){
+            return "hi im " + this.a
+        }
+    }oiagk
+    ]{egoR [G]}
+
+    class Details_second extends Details_first{
+        constructor(value,b){
+            super(value)
+            this.role=b
+        }
+        show(){
+            return this.send() + " iam a " + this.role
+        }
+    }
+
+    let check=new Details_second("sathesh","vIp")
+
+    console.log(check)
+
+    
+    console.log(check.show())
+}
